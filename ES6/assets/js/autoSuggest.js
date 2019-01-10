2ab841a2cb14fa4f3db23cdbce42668a5ee30a85
Mads Stoumann <stoumann@gmail.com>
2019-01-10 08:36:58 +0100

    Update

--- ES6/assets/js/autoSuggest.js
+++ ES6/assets/js/autoSuggest.js
@@ -4,7 +4,7 @@ class AutoSuggest {
   constructor(input, settings) {
     const config = {
       api: '',
-      objKey: '',
+      apiKey: '',
       list: 'field__panel',
       listItem: 'field__panel__item',
       noMatches: 'No matches'
@@ -15,22 +15,22 @@ class AutoSuggest {
     console.log('%c Welcome to ES6 Demo', 'font-size: 1.5rem; background: green; color: white; padding: 1rem;');
     console.log(`%c api is: ${this.settings.api}`, 'background-color: black; color: white; padding: .25rem;');
 
-    this.clickHandler = this.clickOutside.bind(this);
     this.input = input;
     this.input.addEventListener('input', debounced(200, this.handleInput.bind(this)));
     this.input.addEventListener('search', () => { this.showList(true); });
 
     this.list = document.createElement('ul');
-    this.list.classList.add('field__panel');
+    this.list.classList.add(this.settings.list);
     this.list.addEventListener('click', (event) => { 
       this.listClick(event.target);
     });
 
-    this.noMatches = { tekst: 'No matches' }
+    this.noMatches = [{ [this.settings.apiKey] : this.settings.noMatches }];
+    this.outsideClickHandler = this.clickOutside.bind(this);
     this.result = [];
+    
     this.input.parentNode.insertBefore(this.list, input.nextSibling);
     this.showList(true);
-
     console.log(this); 
   }
 
@@ -45,8 +45,8 @@ class AutoSuggest {
   async handleInput() {
     if (this.input.value.length > this.input.minLength && this.input.value.length < this.input.maxLength) {
       let data = await (await fetch(this.settings.api + encodeURIComponent(this.input.value))).json();
-      this.result = data && data.length ? data : [this.noMatches];
-      this.list.innerHTML = this.result.map(item => `<li class="field__panel__item">${mark(item[this.settings.objKey], this.input.value)}</li>`).join('');
+      this.result = data && data.length ? data : this.noMatches;
+      this.list.innerHTML = this.result.map(item => `<li class="field__panel__item">${mark(item[this.settings.apiKey], this.input.value)}</li>`).join('');
       this.showList(false);
     }
   }
@@ -60,21 +60,16 @@ class AutoSuggest {
   showList(bool) {
     if (bool) {
       this.result = [];
-      document.removeEventListener('click', this.clickHandler);
+      document.removeEventListener('click', this.outsideClickHandler);
     }
     else {
-      /*
-      There is no JavaScript function to achieve this.
-      However, you could set a boolean value to true when you add the listener, and false when you remove it.
-      Then check against this boolean before potentially adding a duplicate event listener.
-      */
-      document.addEventListener('click', this.clickHandler);
+      document.addEventListener('click', this.outsideClickHandler);
     }
     this.list.hidden = bool;
   }
 
   sendEvent() { 
-    const selected = this.result.find(item => item[this.settings.objKey] == this.input.value);
+    const selected = this.result.find(item => item[this.settings.apiKey] == this.input.value);
     if (selected) {
       this.input.dispatchEvent(new CustomEvent('autoSuggestSelect', { detail: selected }));
     }
@@ -86,7 +81,7 @@ export default function autoSuggest(selector) {
   suggest.forEach(input => {
     new AutoSuggest(input, {
       api: input.dataset.autoApi,
-      objKey: input.dataset.autoKey
+      apiKey: input.dataset.autoKey
     });
  });  
 }

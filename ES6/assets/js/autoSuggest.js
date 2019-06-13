d45be627eeb706ae8dc6ea18176be3e11bb6371e
Mads Stoumann <stoumann@gmail.com>
2019-06-13 16:15:06 +0200

    SVG

--- ES6/assets/js/autoSuggest.js
+++ ES6/assets/js/autoSuggest.js
@@ -1,4 +1,4 @@
-import { debounced } from './common.js';
+import { debounced, mark } from './common.js';
 
 class AutoSuggest {
   constructor(input, settings) {
@@ -12,29 +12,66 @@ class AutoSuggest {
 
     console.log(`%c Valtech %c FE School %c ES6 `, "background:#333333 ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff", "background:#e60032 ; padding: 1px; color: #fff", "background:#ccc ; padding: 1px; border-radius: 0 3px 3px 0;  color: #222");
     
-    this.settings = {};
-    this.input = input;
-    this.list = { /* Create DOM-node */ };
-    this.result = [];
+    this.settings = { ...config, ...settings };
 
-    console.log(this); 
+    this.input = input;
+    
+    this.bindHandleInput = this.handleInput.bind(this);
+    this.bindClickOutside = this.clickOutside.bind(this);
+
+    this.input.addEventListener('input', debounced(200, this.bindHandleInput));
+    this.input.addEventListener('search', () => { this.hideList(true); });
+
+    this.list = document.createElement('ul');
+    this.list.classList.add(this.settings.cssList);
+    this.list.addEventListener('click', (event) => this.listClick(event.target));
+
+    this.result = [];
+    this.noMatches = [{ [this.settings.apiKey] : this.settings.noResult }];
+
+    this.input.parentNode.insertBefore(this.list, this.input.nextSibling);
+    this.hideList(true);
   }
 
-  handleInput() {
+  clickOutside(event) {
+    if (!this.input.parentNode.contains(event.target)) {
+      this.hideList(true);
+      this.input.value = '';
+    }
+  }
+
+  async handleInput() {
     if (this.input.value.length >= this.input.minLength && this.input.value.length <= this.input.maxLength) {
-      /*
-      TODO: fetch() data, generate markup
-      this.result.map(item => `<li class="${this.settings.cssListItem}">${item}</li>`).join('');
-      */
+      let data = await (await fetch(this.settings.api + encodeURIComponent(this.input.value))).json();
+      this.result = data && data.length ? data : this.noMatches;
+      
+      this.list.innerHTML = this.result.map(item => `<li class="${this.settings.cssListItem}">${mark(item[this.settings.apiKey], this.input.value)}</li>`).join('');
+      this.hideList(false);
     }
   }
 
   hideList(bool) {
-  /* TODO: Some function to hide/show the list with results */
+    if (bool) {
+      this.result = [];
+      document.removeEventListener('click', this.bindClickOutside);
+    }
+    else {
+      document.addEventListener('click', this.bindClickOutside);
+    }
+    this.list.hidden = bool;
+  }
+
+  listClick(element) {
+    this.input.value = element.textContent;
+    this.sendEvent();
+    this.hideList(true);
   }
 
   sendEvent() { 
-  /* TODO: Some custom event with data */
+    const selected = this.result.find(item => item[this.settings.apiKey] == this.input.value);
+    if (selected) {
+      this.input.dispatchEvent(new CustomEvent('autoSuggestSelect', { detail: selected }));
+    }
   }
 }
 
